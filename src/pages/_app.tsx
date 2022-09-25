3cb84859b3df5d2464f40b9dead95d47bb1c05cc
jfernandesdev <jfernandes.dev@gmail.com>
2022-09-25 12:42:50 -0300

    STYLE: global styles and header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+import { globalStyles } from '../styles/global'
+import { Container, Header} from '../styles/pages/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import logoImg from '../assets/logo-ignite-shop.svg'
+
+console.log(logoImg);
+
+globalStyles();
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} alt="" width={logoImg.width} height={logoImg.height} />
+      </Header>
+      
+      <Component {...pageProps} />
+    </Container>
+  )
 }
-
-export default MyApp
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,7 +12,15 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257e6',
+      white: '#FFF',
+
+      gray100: '#E1E1E6',
+      gray300: '#C4C4CC',
+      gray800: '#202024',
+      gray900: '#121214',
+
+      green300: '#00B37E',
+      green500: '#00875F',
     }
   }
 })
